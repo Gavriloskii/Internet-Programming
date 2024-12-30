c052bf006708f71217ff6b0da74d45d320fb7429
FIlip <fgavriloski@gmail.com>
2024-12-30 05:15:47 +0100

    server fix

--- travel-buddy/server.js
+++ travel-buddy/server.js
@@ -15,8 +15,15 @@ app.use('/api', authRoutes); // Use the auth routes under the /api path
 
 const PORT = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 
-// Connect to MongoDB
+console.log('Attempting to connect to MongoDB...');
 mongoose.connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log('Connected to MongoDB successfully.');
+        // Start the server only after successful connection
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
     .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api', matchRoutes); // Use the match routes under the /api path
@@ -24,15 +31,10 @@ app.get('/', (req, res) => {
     res.send('Server is running!');
 });
 
-// Start the server
-const startServer = () => {
-    return app.listen(PORT);
-};
-
 // Function to close the MongoDB connection
 const closeMongoConnection = () => {
     return mongoose.connection.close();
 };
 
 // Export the app, start function, and close function for testing
-module.exports = { app, startServer, closeMongoConnection };
+module.exports = { app, closeMongoConnection };
